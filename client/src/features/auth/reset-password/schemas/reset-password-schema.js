3f95a3477ef66b0a9b71ae4da0610f0a78be99dd
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2026-01-19 22:18:42 +0700

    fix: reset password in auth

--- client/src/features/auth/reset-password/schemas/reset-password-schema.js
+++ client/src/features/auth/reset-password/schemas/reset-password-schema.js
@@ -3,11 +3,11 @@ import * as yup from 'yup';
 export const resetPasswordSchema = yup.object({
   password: yup
     .string()
-    .min(6, 'Password must be at least 6 characters long')
-    .required('Password is required'),
+    .min(6, 'Mật khẩu phải có ít nhất 6 ký tự')
+    .required('Vui lòng nhập mật khẩu'),
   confirmPassword: yup
     .string()
-    .min(6, 'Confirm Password must be at least 6 characters long')
-    .oneOf([yup.ref('password')], "Passwords don't match")
-    .required('Confirm Password is required')
+    .min(6, 'Xác nhận mật khẩu phải có ít nhất 6 ký tự')
+    .oneOf([yup.ref('password')], 'Mật khẩu không khớp')
+    .required('Vui lòng nhập xác nhận mật khẩu')
 });
