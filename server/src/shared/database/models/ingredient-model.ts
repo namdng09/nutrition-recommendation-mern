5c3f846f901d02dbaf2c8ec8562c2e139f878be4
namdng09 <namduong090104@gmail.com>
2026-01-28 01:07:07 +0700

    feat: update nutrition detail schema have array fields

--- server/src/shared/database/models/ingredient-model.ts
+++ server/src/shared/database/models/ingredient-model.ts
@@ -4,98 +4,101 @@ import mongoose, {
   Schema
 } from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
-import { ALLERGEN } from '~/shared/constants/allergen';
+import { required } from 'zod/v4/core/util.cjs';
 
+import { ALLERGEN } from '~/shared/constants/allergen';
+import { NUTRITION_AMINO_ACID } from '~/shared/constants/nutrition-amino-acid';
+import { NUTRITION_FAT } from '~/shared/constants/nutrition-fat';
+import { NUTRITION_FATTY_ACID } from '~/shared/constants/nutrition-fatty-acid';
+import { NUTRITION_MINERAL } from '~/shared/constants/nutrition-minerals';
+import { NUTRITION_SUGAR } from '~/shared/constants/nutrition-sugar';
+import { NUTRITION_VITAMIN } from '~/shared/constants/nutrition-vitamin';
 import { UNIT } from '~/shared/constants/unit';
 
-const nutritionMacrosSchema = new Schema(
+const nutritionSchema = new Schema(
   {
-    carbs: { type: Number },
-    protein: { type: Number },
-    fat: { type: Number },
-    netCarbs: { type: Number },
-    fiber: { type: Number }
-  },
-  { _id: false }
-);
-
-const nutritionMineralsSchema = new Schema(
-  {
-    sodium: { type: Number },
-    calcium: { type: Number },
-    iron: { type: Number },
-    potassium: { type: Number },
-    magnesium: { type: Number },
-    phosphorus: { type: Number },
-    zinc: { type: Number },
-    selenium: { type: Number }
-  },
-  { _id: false }
-);
-
-const nutritionVitaminsSchema = new Schema(
-  {
-    vitaminA: { type: Number },
-    vitaminD: { type: Number },
-    vitaminC: { type: Number },
-    vitaminE: { type: Number },
-    vitaminK: { type: Number },
-    vitaminB1: { type: Number },
-    vitaminB2: { type: Number },
-    vitaminB3: { type: Number },
-    vitaminB5: { type: Number },
-    vitaminB6: { type: Number },
-    vitaminB9: { type: Number },
-    vitaminB12: { type: Number }
-  },
-  { _id: false }
-);
-
-const nutritionCompoundsSchema = new Schema(
-  {
-    cholesterol: { type: Number },
-    caffeine: { type: Number },
-    choline: { type: Number },
-    lycopene: { type: Number },
-    betaCarotene: { type: Number }
-  },
-  { _id: false }
-);
-
-export const nutritionSchema = new Schema(
-  {
-    units: {
-      calories: {
+    calories: {
+      value: { type: Number, required: true },
+      unit: {
         type: String,
         enum: Object.values(UNIT),
-        default: UNIT.KILOCALORIE
-      },
-      macronutrients: {
-        type: String,
-        enum: Object.values(UNIT),
-        default: UNIT.GRAM
-      },
-      minerals: {
-        type: String,
-        enum: Object.values(UNIT),
-        default: UNIT.MILLIGRAM
-      },
-      vitamins: {
-        type: String,
-        enum: Object.values(UNIT),
-        default: UNIT.MICROGRAM
-      },
-      otherCompounds: {
-        type: String,
-        enum: Object.values(UNIT),
-        default: UNIT.MILLIGRAM
+        default: UNIT.KILOCALORIE,
+        required: true
       }
     },
-    calories: { type: Number },
-    macronutrients: { type: nutritionMacrosSchema },
-    minerals: { type: nutritionMineralsSchema },
-    vitamins: { type: nutritionVitaminsSchema },
-    otherCompounds: { type: nutritionCompoundsSchema }
+    carbs: {
+      value: { type: Number, required: true },
+      unit: { type: String, enum: Object.values(UNIT), required: true }
+    },
+    fat: {
+      value: { type: Number, required: true },
+      unit: { type: String, enum: Object.values(UNIT), required: true }
+    },
+    protein: {
+      value: { type: Number, required: true },
+      unit: { type: String, enum: Object.values(UNIT), required: true }
+    },
+    fiber: {
+      value: { type: Number, required: true },
+      unit: { type: String, enum: Object.values(UNIT), required: true }
+    },
+    sodium: {
+      value: { type: Number, required: true },
+      unit: { type: String, enum: Object.values(UNIT), required: true }
+    },
+    cholesterol: {
+      value: { type: Number, required: true },
+      unit: { type: String, enum: Object.values(UNIT), required: true }
+    }
+  },
+  { _id: false }
+);
+
+export const detailNutritionSchema = new Schema(
+  {
+    macronutrients: { type: nutritionSchema },
+    minerals: [
+      {
+        label: { type: String, enum: Object.values(NUTRITION_MINERAL) },
+        value: { type: Number, min: 0 },
+        unit: { type: String, enum: Object.values(UNIT), required: true }
+      }
+    ],
+    vitamins: [
+      {
+        label: { type: String, enum: Object.values(NUTRITION_VITAMIN) },
+        value: { type: Number, min: 0 },
+        unit: { type: String, enum: Object.values(UNIT), required: true }
+      }
+    ],
+    sugars: [
+      {
+        label: { type: String, enum: Object.values(NUTRITION_SUGAR) },
+        value: { type: Number, min: 0 },
+        unit: { type: String, enum: Object.values(UNIT), required: true }
+      }
+    ],
+    fats: [
+      {
+        label: { type: String, enum: Object.values(NUTRITION_FAT) },
+        value: { type: Number, min: 0 },
+        unit: { type: String, enum: Object.values(UNIT), required: true }
+      }
+    ],
+    fattyAcids: [
+      {
+        label: { type: String, enum: Object.values(NUTRITION_FATTY_ACID) },
+        value: { type: Number, min: 0 },
+        unit: { type: String, enum: Object.values(UNIT), required: true }
+      }
+    ],
+    aminoAcids: [
+      {
+        label: { type: String, enum: Object.values(NUTRITION_AMINO_ACID) },
+        value: { type: Number, min: 0 },
+        unit: { type: String, enum: Object.values(UNIT), required: true }
+      }
+    ]
   },
   { _id: false }
 );
@@ -117,7 +120,7 @@ const ingredientSchema = new Schema(
       }
     ],
     allergens: [{ type: String, enum: Object.values(ALLERGEN) }],
-    nutrition: { type: nutritionSchema },
+    nutrition: { type: detailNutritionSchema },
     isActive: { type: Boolean, default: true }
   },
   {
