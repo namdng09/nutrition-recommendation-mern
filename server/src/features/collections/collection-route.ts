88b195050f6695e6d6808933f38e01bcc3e8ec2d
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2026-01-28 14:04:12 +0700

    chore: remove unused APIs

--- server/src/features/collections/collection-route.ts
+++ server/src/features/collections/collection-route.ts
@@ -19,77 +19,59 @@ import {
 
 const router = Router();
 
-// Create a new collection
 router.post(
   '/',
   authenticate(),
+  authorize([ROLE.NUTRITIONIST]),
   handleSingleImageUpload('image'),
   validate(createCollectionRequestSchema.shape),
   asyncHandler(CollectionController.createCollection)
 );
 
-// Get all public collections
 router.get('/', asyncHandler(CollectionController.viewCollections));
 
-// Get my collections
-router.get(
-  '/me',
-  authenticate(),
-  asyncHandler(CollectionController.viewMyCollections)
-);
-
-// Delete multiple collections
-router.delete(
-  '/',
-  authenticate(),
-  asyncHandler(CollectionController.deleteBulk)
-);
-
-// Get collection detail
 router.get('/:id', asyncHandler(CollectionController.viewCollectionDetail));
 
-// Update collection
 router.put(
   '/:id',
   authenticate(),
+  authorize([ROLE.NUTRITIONIST]),
   handleSingleImageUpload('image'),
   validate(updateCollectionRequestSchema.shape),
   asyncHandler(CollectionController.updateCollection)
 );
 
-// Delete collection
 router.delete(
   '/:id',
   authenticate(),
+  authorize([ROLE.NUTRITIONIST]),
   asyncHandler(CollectionController.deleteCollection)
 );
 
-// Add dish to collection
 router.post(
   '/:id/dishes',
   authenticate(),
+  authorize([ROLE.NUTRITIONIST]),
   parseFormData,
   validate(addDishToCollectionRequestSchema.shape),
   asyncHandler(CollectionController.addDishToCollection)
 );
 
-// Remove dish from collection
 router.delete(
   '/:id/dishes',
   authenticate(),
+  authorize([ROLE.NUTRITIONIST]),
   parseFormData,
   validate(removeDishFromCollectionRequestSchema.shape),
   asyncHandler(CollectionController.removeDishFromCollection)
 );
 
-// Follow collection
 router.post(
   '/:id/follow',
   authenticate(),
   asyncHandler(CollectionController.followCollection)
 );
 
-// Unfollow collection
 router.delete(
   '/:id/follow',
   authenticate(),
