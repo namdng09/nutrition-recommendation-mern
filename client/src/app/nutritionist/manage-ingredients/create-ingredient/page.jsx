a1cd9d53843d95acd9548b4716cb2786ce485763
Le Nguyen Viet Hung <lenguyenviethung1509@gmail.com>
2026-01-29 06:05:56 +0700

    feat: create ingredient page for nutritionist

---
+++ client/src/app/nutritionist/manage-ingredients/create-ingredient/page.jsx
@@ -0,0 +1,10 @@
+import CreateIngredientForm from '~/features/ingredients/create-ingredient/components/nutritionist/create-ingredient-form';
+const Page = () => {
+  return (
+    <div className='container mx-auto py-8 px-4'>
+      <CreateIngredientForm />
+    </div>
+  );
+};
+
+export default Page;
