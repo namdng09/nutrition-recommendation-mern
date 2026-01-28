878b95e02067903c64ac69c378dee769e526939e
Le Nguyen Viet Hung <lenguyenviethung1509@gmail.com>
2026-01-29 02:32:27 +0700

    feat: add nutritionist role

--- client/src/constants/role.js
+++ client/src/constants/role.js
@@ -1,9 +1,11 @@
 export const ROLE = Object.freeze({
   USER: 'user',
+  NUTRITIONIST: 'nutritionist',
   ADMIN: 'admin'
 });
 
 export const ROLE_OPTIONS = [
   { value: ROLE.USER, label: 'User' },
+  { value: ROLE.NUTRITIONIST, label: 'Nutritionist' },
   { value: ROLE.ADMIN, label: 'Admin' }
 ];
