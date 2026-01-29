19642cd18176d5c7103848a61a18375747535432
dangph12 <dangphhe181217@fpt.edu.vn>
2026-01-29 09:26:59 +0700

    feat: add more constants

---
+++ client/src/constants/meal-complexity.js
@@ -0,0 +1,13 @@
+export const MEAL_COMPLEXITY = Object.freeze({
+  SUPER_SIMPLE: 'Rất đơn giản',
+  SIMPLE: 'Đơn giản',
+  MODERATE: 'Trung bình',
+  COMPLEX: 'Phức tạp'
+});
+
+export const MEAL_COMPLEXITY_OPTIONS = [
+  { value: MEAL_COMPLEXITY.SUPER_SIMPLE, label: 'Rất đơn giản' },
+  { value: MEAL_COMPLEXITY.SIMPLE, label: 'Đơn giản' },
+  { value: MEAL_COMPLEXITY.MODERATE, label: 'Trung bình' },
+  { value: MEAL_COMPLEXITY.COMPLEX, label: 'Phức tạp' }
+];
