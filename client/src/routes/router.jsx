434b9768222425d02115595e244e6223c5c430bc
Le Nguyen Viet Hung <lenguyenviethung1509@gmail.com>
2026-01-29 15:30:53 +0700

    feat: nutritionist route

--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -76,17 +76,19 @@ const router = createBrowserRouter([
       },
       {
         path: '/nutritionist/',
-        // Component: () => (
-        //   <PrivateRoute allowedRoles={[ROLE.NUTRITIONIST]}>
-        //     <AdminLayout />
-        //   </PrivateRoute>
-        // ),
-        Component: AdminLayout,
+        Component: () => (
+          <PrivateRoute allowedRoles={[ROLE.NUTRITIONIST]}>
+            <AdminLayout />
+          </PrivateRoute>
+        ),
+        // Component: AdminLayout,
         children: [
           {
             path: '',
             Component: lazy(() => import('~/app/nutritionist/page'))
           },
+
+          //Manage ingredients
           {
             path: 'manage-ingredients/',
             Component: lazy(
@@ -106,6 +108,14 @@ const router = createBrowserRouter([
                 import(
                   '~/app/nutritionist/manage-ingredients/create-ingredient/page'
                 )
+            )
+          },
+
+          // Manage dishes
+          {
+            path: 'manage-dishes/',
+            Component: lazy(
+              () => import('~/app/nutritionist/manage-dishes/page')
             )
           }
         ]
