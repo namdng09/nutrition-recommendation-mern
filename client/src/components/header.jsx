07c52284333820dfa7955f2ecfba3d4c1adadb66
dangph12 <dangphhe181217@fpt.edu.vn>
2026-02-01 16:20:57 +0700

    fix: fix logo overlap profile sidebar

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -57,11 +57,11 @@ const Header = () => {
             to='/'
             className='group flex items-center gap-3 transition-all hover:opacity-90'
           >
-            <div className='relative flex h-20 w-20 items-center justify-center bg-transparent transition-transform duration-500 group-hover:scale-110'>
+            <div className='relative flex h-16 w-16 items-center justify-center overflow-hidden rounded-lg transition-transform duration-300 group-hover:scale-105'>
               <img
                 src='/logo2.png'
                 alt='Logo'
-                className='h-full w-full object-contain scale-[1.8]'
+                className='h-full w-full object-contain'
               />
             </div>
 
