144a3d86d3a71b86267a8826d3815b020f4311d8
dangph12 <dangphhe181217@fpt.edu.vn>
2026-02-02 10:43:09 +0700

    feat: add decorator to constants

--- client/src/constants/diet.js
+++ client/src/constants/diet.js
@@ -1,3 +1,5 @@
+import { ChefHat, Drumstick, Leaf, Pizza, Salad, Wheat } from 'lucide-react';
+
 export const DIET = Object.freeze({
   ANYTHING: 'Ăn uống tự do',
   KETO: 'Keto',
@@ -8,10 +10,10 @@ export const DIET = Object.freeze({
 });
 
 export const DIET_OPTIONS = [
-  { value: DIET.ANYTHING, label: 'Ăn uống tự do' },
-  { value: DIET.KETO, label: 'Keto' },
-  { value: DIET.MEDITERRANEAN, label: 'Địa Trung Hải' },
-  { value: DIET.PALEO, label: 'Paleo' },
-  { value: DIET.VEGAN, label: 'Thuần chay' },
-  { value: DIET.VEGETARIAN, label: 'Ăn chay' }
+  { value: DIET.ANYTHING, label: 'Ăn uống tự do', icon: Pizza },
+  { value: DIET.KETO, label: 'Keto', icon: Wheat },
+  { value: DIET.MEDITERRANEAN, label: 'Địa Trung Hải', icon: Salad },
+  { value: DIET.PALEO, label: 'Paleo', icon: Drumstick },
+  { value: DIET.VEGAN, label: 'Thuần chay', icon: Leaf },
+  { value: DIET.VEGETARIAN, label: 'Ăn chay', icon: ChefHat }
 ];
