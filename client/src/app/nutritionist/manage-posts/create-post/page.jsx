58cf926a5db817fd26ae4a5fe4ba7c0713fb02e7
Le Nguyen Viet Hung <lenguyenviethung1509@gmail.com>
2026-02-05 02:17:14 +0700

    feat: manage post

---
+++ client/src/app/nutritionist/manage-posts/create-post/page.jsx
@@ -0,0 +1,10 @@
+import CreatePostForm from '~/features/post/create-post/components/nutritionist/create-post-form';
+const Page = () => {
+  return (
+    <div className='container mx-auto py-8 px-4'>
+      <CreatePostForm />
+    </div>
+  );
+};
+
+export default Page;
