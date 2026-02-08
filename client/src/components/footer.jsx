94bde3bccb06c3aeffbb5eec5d4c2403768cf67e
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2026-02-08 19:07:58 +0700

    feat: view grocery api

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaHome, FaUserPlus } from 'react-icons/fa';
+import { FaHome } from 'react-icons/fa';
 import { Link } from 'react-router';
 
 const Footer = () => {
