1fcedb0d89807bdcce8a9293db1e3233715e241f
namdng09 <namduong090104@gmail.com>
2026-02-10 09:32:01 +0700

    fix: update ingredient and dish model

--- server/src/shared/constants/nutrition-minerals.ts
+++ server/src/shared/constants/nutrition-minerals.ts
@@ -1,23 +1,14 @@
 export const NUTRITION_MINERAL = {
-  ALPHA_CAROTENE: 'Alpha carotene',
-  BETA_CAROTENE: 'Beta carotene',
-  CAFFEINE: 'Caffeine',
-  CHOLINE: 'Choline',
-  COPPER: 'Copper',
-  FLUORIDE: 'Fluoride',
-  FOLATE_B9: 'Folate (B9)',
-  LYCOPENE: 'Lycopene',
-  MAGNESIUM: 'Magnesium',
-  MANGANESE: 'Manganese',
-  NIACIN: 'Niacin',
-  PANTOTHENIC_ACID: 'Pantothenic acid',
-  PHOSPHORUS: 'Phosphorus',
-  RETINOL: 'Retinol',
-  RIBOFLAVIN_B2: 'Riboflavin (B2)',
-  SELENIUM: 'Selenium',
-  THEOBROMINE: 'Theobromine',
-  THIAMINE: 'Thiamine',
-  ZINC: 'Zinc'
+  CALCI: 'Calci',
+  SAT: 'Såt',
+  MAGIE: 'Magiê',
+  MANGAN: 'Mangan',
+  PHOSPHO: 'Phospho',
+  KALI: 'Kali',
+  NATRI: 'Natri',
+  KEM: 'Kẽm',
+  DONG: 'Đồng',
+  SELEN: 'Selen'
 } as const;
 
 export type NutritionMineral =
