398a3541118c9c1cc455d86cd6cfc258329ce896
lnvh <lenguyenviethung1509@gmail.com>
2026-03-16 14:47:25 +0700

    fix: update schedule setting in user profile

--- client/src/components/profile-sidebar.jsx
+++ client/src/components/profile-sidebar.jsx
@@ -112,8 +112,8 @@ export function ProfileSidebar({ ...props }) {
     <>
       {/* Mobile Toggle Button - Fixed Position */}
       {isMobile && (
-        <div className='fixed top-20 z-50 md:hidden'>
-          <SidebarTrigger className='h-10 w-10 rounded-full bg-primary text-primary-foreground shadow-lg '>
+        <div className='fixed left-0 top-20 z-50 md:hidden'>
+          <SidebarTrigger className='h-10 w-10 -translate-x-1/2 rounded-full bg-primary text-primary-foreground shadow-lg'>
             <Menu className='h-5 w-5' />
           </SidebarTrigger>
         </div>
