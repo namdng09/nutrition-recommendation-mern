fc97d66ac02ed93fdd4b1cf02685a3466f442020
dangph12 <dangphhe181217@fpt.edu.vn>
2026-03-18 19:05:19 +0700

    fix: set cookie secure flag

--- server/src/features/auth/auth-controller.ts
+++ server/src/features/auth/auth-controller.ts
@@ -15,7 +15,7 @@ export const AuthController = {
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.SERVER_URL?.startsWith('https'),
       sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
@@ -37,7 +37,7 @@ export const AuthController = {
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.SERVER_URL?.startsWith('https'),
       sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
@@ -60,7 +60,7 @@ export const AuthController = {
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.SERVER_URL?.startsWith('https'),
       sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
@@ -76,7 +76,7 @@ export const AuthController = {
   logout: async (req: Request, res: Response) => {
     res.clearCookie('refreshToken', {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.SERVER_URL?.startsWith('https'),
       sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none'
     });
 
--- server/src/shared/middlewares/error-handler.ts
+++ server/src/shared/middlewares/error-handler.ts
@@ -16,7 +16,7 @@ export const errorHandler: ErrorRequestHandler = (
   if (err instanceof Error && err.message === 'Refresh token expired') {
     res.clearCookie('refreshToken', {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.SERVER_URL?.startsWith('https'),
       sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none'
     });
   }
