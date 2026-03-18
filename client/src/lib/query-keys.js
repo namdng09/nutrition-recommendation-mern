bc1a9e8b5216fb187747bba89fe893bd186a5ce3
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2026-03-19 05:41:52 +0700

    fix: query key schedule

--- client/src/lib/query-keys.js
+++ client/src/lib/query-keys.js
@@ -11,7 +11,7 @@ export const QUERY_KEYS = {
   SCHEDULES: ['schedules'],
   POSTS: ['posts'],
   POST: id => ['post', id],
-  SCHEDULE: id => ['schedules', id],
+  SCHEDULE: id => ['schedule', id],
   DISH_NUTRITION_DETAIL: id => ['dish_nutrition_detail', id],
   GROCERIES: ['groceries'],
   PAYMENTS: ['payments'],
