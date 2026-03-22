e15be2736e93278d8551a2597f6f9321b5f3f1c4
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2026-03-22 09:54:48 +0700

    feat: related ingredient horizontal component

--- client/src/features/ingredients/view-ingredients-detail/components/ingredient-detail.jsx
+++ client/src/features/ingredients/view-ingredients-detail/components/ingredient-detail.jsx
@@ -3,16 +3,19 @@ import { useNavigate, useParams } from 'react-router';
 
 import { findByLabel } from '~/lib/utils';
 
+import { useIngredients } from '../../view-ingredients/api/view-ingredient';
 import { useIngredientDetail } from '../api/view-ingredient-detail';
 import IngredientDetailHeader from './ingredient-detail-header';
 import IngredientMainInfo from './ingredient-main-info';
 import IngredientNutritionModal from './ingredient-nutrition-model';
 import IngredientNutritionSection from './ingredient-nutrition-section';
+import RelatedIngredientsHorizontal from './related-ingredients-horizontal';
 
 export default function IngredientDetail() {
   const navigate = useNavigate();
   const { id } = useParams();
   const { data } = useIngredientDetail(id);
+  const { data: ingredientsData } = useIngredients();
 
   const item = data;
   const defaultUnit = item?.units?.find(u => u.isDefault) || item?.units?.[0];
@@ -28,6 +31,8 @@ export default function IngredientDetail() {
   const sodium = findByLabel(mineralList, 'Natri');
 
   const [openNutrition, setOpenNutrition] = useState(false);
+
+  const allIngredients = ingredientsData?.docs || [];
 
   return (
     <div className='mx-auto w-full max-w-8xl space-y-5'>
@@ -48,6 +53,13 @@ export default function IngredientDetail() {
 
         <IngredientNutritionSection item={item} />
       </div>
+
+      <RelatedIngredientsHorizontal
+        title='Nguyên liệu tương tự'
+        description='Các nguyên liệu có nhóm hoặc đặc điểm gần giống'
+        currentIngredient={item}
+        allIngredients={allIngredients}
+      />
 
       <IngredientNutritionModal
         open={openNutrition}
