3161d0daf1767b8829f939910955b8f91c6256f0
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2026-04-14 05:11:24 +0700

    refactor: change title and content must have at least 2 characters

--- server/src/features/posts/post-dto.ts
+++ server/src/features/posts/post-dto.ts
@@ -7,11 +7,11 @@ export const createPostRequestSchema = z.object({
   title: z
     .string('Tiêu đề không hợp lệ')
     .trim()
-    .min(5, 'Tiêu đề phải có ít nhất 5 ký tự'),
+    .min(2, 'Tiêu đề phải có ít nhất 2 ký tự'),
   content: z
     .string('Nội dung không hợp lệ')
     .trim()
-    .min(10, 'Nội dung phải có ít nhất 10 ký tự'),
+    .min(2, 'Nội dung phải có ít nhất 2 ký tự'),
   tags: z.preprocess(parseJSON, z.array(z.string().trim())).optional(),
   category: z.enum(Object.values(POST_CATEGORY)).optional(),
   images: z.array(z.file()).optional(),
@@ -28,7 +28,7 @@ export const createCommentRequestSchema = z.object({
   content: z
     .string('Nội dung bình luận không hợp lệ')
     .trim()
-    .min(1, 'Nội dung bình luận phải có ít nhất 1 ký tự')
+    .min(2, 'Nội dung bình luận phải có ít nhất 2 ký tự')
 });
 
 export type CreateCommentRequest = z.infer<typeof createCommentRequestSchema>;
